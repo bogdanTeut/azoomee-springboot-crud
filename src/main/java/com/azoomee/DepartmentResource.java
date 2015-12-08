f4e9be4ab4c96aa3adb89eeaa2510cb501696b61
bogdanTeut <bogdan_teut@yahoo.com>
2015-12-08 10:15:04 +0000

    first commit

---
+++ src/main/java/com/azoomee/DepartmentResource.java
@@ -0,0 +1,11 @@
+package com.azoomee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentResource {
+
+    @RequestMapping("/department/{id}")
+    public String
+}
